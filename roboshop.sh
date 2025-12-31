0ccc2113e2f8e2fdab458f94ae9c3b365494e310
raviteja <ravitejaraju1999@gmail.com>
2025-12-31 18:49:31 +0530

    setup instance using shell

--- roboshop.sh
+++ roboshop.sh
@@ -7,13 +7,24 @@ for instance in $@
 
 do
 
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0eceac836e52f5116 --tag-specifications 
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications 
     "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
 
     # Get Private Ip
     if [ $instance != "frontend" ]; then
-        
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+
+    fi
+
+    echo "$instance: $IP"
+
+done
+
+
+
 
 
 
