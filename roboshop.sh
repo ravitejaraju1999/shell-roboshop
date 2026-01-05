a30797bf9238481aa07e11be49aa6ad451c0b7dd
raviteja <ravitejaraju1999@gmail.com>
2026-01-05 21:00:30 +0530

    setup instance using shell

--- roboshop.sh
+++ roboshop.sh
@@ -2,6 +2,8 @@
 
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-0eceac836e52f5116"
+ZONE_ID="Z09554163BEIVGQB3DW1S"
+DOMAIN_NAME="raviteja.store"
 
 for instance in $@
 
@@ -13,12 +15,34 @@ do
     # Get Private Ip
     if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.raviteja.store
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        RECORD_NAME="$DOMAIN_NAME" #raviteja.store
 
     fi
 
     echo "$instance: $IP"
+
+
+    aws route53 change-resource-record-sets \
+  --hosted-zone-id $ZONE_ID \
+  --change-batch '
+    {
+        "Comment": "Updating record set",
+        "Changes": [{
+        "Action": "UPSERT",
+        "ResourceRecordSet": {
+            "Name": "'$RECORD_NAME'",
+            "Type": "A",
+            "TTL": 1,
+            "ResourceRecords": [{
+            "Value": "'$IP'"
+            }]
+        }
+        }]
+    }'
+
 
 done
 
