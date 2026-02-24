b88e017dbd994f67e5d070ab7f447d8bc42da455
raviteja <ravitejaraju1999@gmail.com>
2026-02-25 00:37:45 +0530

    with sed commands

--- mongodb.sh
+++ mongodb.sh
@@ -39,3 +39,10 @@ VALIDATE $? "Enable MongoDB"
 systemctl start mongod &>>$LOG_FILE
 VALIDATE $? "Start MongoDB"
 
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+
+VALIDATE $? "allowing remote connections to MongoDB"
+
+
+systemctl restart mongodb
+VALIDATE $? "Restarted MongoDB"
