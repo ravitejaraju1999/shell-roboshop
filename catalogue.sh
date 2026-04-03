3a1c51c45534be1341e87df87d58ec49e13e21db
raviteja <ravitejaraju1999@gmail.com>
2026-04-03 20:06:33 +0530

    resolved filepath

--- catalogue.sh
+++ catalogue.sh
@@ -38,10 +38,18 @@ VALIDATE $? "enabling nodeJS"
 dnf install nodejs -y &>>$LOG_FILE
 VALIDATE $? "Installing NodeJS"
 
-useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
-VALIDATE $? "creating system user"
 
-mkdir /app
+id roboshop
+
+if [ $? -ne 0]; then
+
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+    VALIDATE $? "creating system user"
+else
+    echo "user already exist ...$Y skipping $N"
+fi
+
+mkdir -p /app
 VALIDATE $? "creating app directory"
 
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip  &>>$LOG_FILE
