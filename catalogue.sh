abf0842c8f58e6c2aff8a891c4a0721d6046a8e6
raviteja <ravitejaraju1999@gmail.com>
2026-04-03 16:49:04 +0530

    resolved filepath

--- catalogue.sh
+++ catalogue.sh
@@ -8,8 +8,8 @@ N="\e[0m"
 
 LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
-SCRIPT_DIR = $PWD
-MONGODB_HOST = mongodb.raviteja.store
+SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.raviteja.store
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 
 mkdir -p $LOGS_FOLDER
